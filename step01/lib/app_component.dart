27ed70fbebf5ca8d359670ad8afbe762332ca14d
Takayuki Hayashi <takayuki.hayashi@uzabase.com>
2018-02-09 02:04:10 +0900

    add input

--- step01/lib/app_component.dart
+++ step01/lib/app_component.dart
@@ -1,11 +1,15 @@
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
+import 'package:step01/src/component_center.dart';
+import 'package:step01/src/component_left.dart';
+import 'package:step01/src/component_list.dart';
+import 'package:step01/src/component_right.dart';
 
 @Component(
   selector: 'my-app',
   styleUrls: const ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: const [materialDirectives],
+  directives: const [materialDirectives,ComponentCenter, ComponentLeft, ComponentRight, ComponentList],
   providers: const [materialProviders],
 )
 class AppComponent {
--- step01/lib/src/component_center.dart
+++ step01/lib/src/component_center.dart
@@ -1,13 +1,23 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
+import 'package:angular_forms/angular_forms.dart';
 
 @Component(
   selector: 'component-center',
   templateUrl: 'component_center.html',
-  styleUrls: const ['component_center.css'])
+  styleUrls: const ['component_center.css'],
+  directives: const [materialDirectives,CORE_DIRECTIVES, formDirectives]
+)
 class ComponentCenter implements OnInit {
+
+  String input;
 
   ComponentCenter();
 
   @override
   void ngOnInit() {}
+
+  void onChange() {
+    print(input);
+  }
 }
